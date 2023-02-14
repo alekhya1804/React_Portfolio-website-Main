1eec88d74b3c29db45e1490b332fa96abc5fcbe6
alphabeZ <54945906+alekhya1804@users.noreply.github.com>
2023-02-14 14:01:25 +0000

    I din't change anything

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -1,15 +1,15 @@
 import styles from "./homepage.module.css"
 import { HashLink as Link } from 'react-router-hash-link'
-import ResumePDF from '../../files/Elnatan.pdf';
+import ResumePDF from '../../files/Alekhya_Reddy.pdf';
 
 const HomePage = () => {
   return(
     <>
       <div className={styles.homepage}>
-        <h1 className={styles.myname}>Elnatan Samuel</h1>
+        <h1 className={styles.myname}>Alekhya Reddy</h1>
         <p className={styles.paragraph}>Front-end web developer</p>
         <div className={styles.btn}>
-          <a target="_blank" href={ResumePDF} download="Elnatan_Samuel.pdf"><button className={styles.btn1}>Resume</button></a>
+          <a target="_blank" href={ResumePDF} download="Alekhya_Reddy.pdf"><button className={styles.btn1}>Resume</button></a>
           <Link smooth style={{ color: 'inherit', textDecoration: 'inherit'}} to="#contact"><button className={styles.btn2}>Contact</button></Link>
         </div>
       </div>
